04ea51a7d6bac5c635b9f16ce00b3d86520355bb
Moazzam Khan <moazzamk@gmail.com>
2018-06-10 03:03:45 -0500

    Fixed an issue where submitting a new order would cause "Precision is over the maximum defined for this asset" error.
    NewOrderRequest now has a precision field which can be used to set the precision

--- service_account.go
+++ service_account.go
@@ -86,13 +86,18 @@ func (as *apiService) NewOrder(or NewOrderRequest) (*ProcessedOrder, error) {
 }
 
 func (as *apiService) NewOrderTest(or NewOrderRequest) error {
+	precision := 10
+	if or.PricePrecision != 0 {
+		precision = or.PricePrecision
+	}
+
 	params := make(map[string]string)
 	params["symbol"] = or.Symbol
 	params["side"] = string(or.Side)
 	params["type"] = string(or.Type)
 	params["timeInForce"] = string(or.TimeInForce)
 	params["quantity"] = strconv.FormatFloat(or.Quantity, 'f', 10, 64)
-	params["price"] = strconv.FormatFloat(or.Price, 'f', 10, 64)
+	params["price"] = strconv.FormatFloat(or.Price, 'f', precision, 64)
 	params["timestamp"] = strconv.FormatInt(unixMillis(or.Timestamp), 10)
 	if or.NewClientOrderID != "" {
 		params["newClientOrderId"] = or.NewClientOrderID
